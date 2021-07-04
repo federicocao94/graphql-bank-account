9aa72f6fd2c4538f9bde2eaf82100a977d3514a7
federicocao <federico.cao94@gmail.com>
2021-07-05 00:57:01 +0200

    Adding cursors pagination for bank accounts

---
+++ src/main/java/com/example/graphqldemo/repository/BankAccountRepository.java
@@ -0,0 +1,58 @@
+package com.example.graphqldemo.repository;
+
+import com.example.graphqldemo.domain.BankAccount;
+import com.example.graphqldemo.domain.Currency;
+import org.springframework.stereotype.Component;
+
+import java.time.ZonedDateTime;
+import java.util.Comparator;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+//mock for bank accounts
+@Component
+public class BankAccountRepository {
+
+    private final List<BankAccount> bankAccounts = List.of(
+            BankAccount.builder()
+                    .id(UUID.fromString("a418a904-351c-4f32-b763-96e15515d7c4"))
+                    .name("test1")
+                    .currency(Currency.USD)
+                    .createdAt(ZonedDateTime.parse("2021-07-04T23:30:00+00:00"))
+                    .build(),
+            BankAccount.builder()
+                    .id(UUID.fromString("f835d1a3-012a-4224-8621-346bc38f1d51"))
+                    .name("test2")
+                    .currency(Currency.USD)
+                    .createdAt(ZonedDateTime.parse("2021-07-04T23:30:00+00:00"))
+                    .build(),
+            BankAccount.builder()
+                    .id(UUID.fromString("24cd7105-0608-4fc1-bd15-131185f24407"))
+                    .name("test3")
+                    .currency(Currency.USD)
+                    .createdAt(ZonedDateTime.parse("2021-07-04T23:30:00+00:00"))
+                    .build(),
+            BankAccount.builder()
+                    .id(UUID.fromString("42e53878-898e-45e4-aa35-0047e1412032"))
+                    .name("test4")
+                    .currency(Currency.USD)
+                    .createdAt(ZonedDateTime.parse("2021-07-04T23:30:00+00:00"))
+                    .build()
+    ).stream()
+            .sorted(Comparator.comparing(BankAccount::getId))
+            .collect(Collectors.toList());
+
+
+    public List<BankAccount> getBankAccounts() {
+        return bankAccounts;
+    }
+
+
+    public List<BankAccount> getBankAccountsAfter(UUID id) {
+        return bankAccounts.stream()
+                .dropWhile(bankAccount -> bankAccount.getId().compareTo(id) != 1)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+}
--- src/main/java/com/example/graphqldemo/resolver/bank/query/BankAccountResolver.java
+++ src/main/java/com/example/graphqldemo/resolver/bank/query/BankAccountResolver.java
@@ -1,17 +1,29 @@
 package com.example.graphqldemo.resolver.bank.query;
 
+import com.example.graphqldemo.connection.CursorUtil;
 import com.example.graphqldemo.domain.BankAccount;
-import com.example.graphqldemo.domain.Client;
 import com.example.graphqldemo.domain.Currency;
+import com.example.graphqldemo.repository.BankAccountRepository;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import graphql.relay.*;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class BankAccountResolver implements GraphQLQueryResolver {
+
+    private final BankAccountRepository bankAccountRepository;
+
+    private final CursorUtil cursorUtil;
+
 
     public BankAccount bankAccount(UUID id) {
         log.info("Retrieving bank account id: {}", id);
@@ -21,6 +33,34 @@ public class BankAccountResolver implements GraphQLQueryResolver {
                 .currency(Currency.EUR)
                 .name("Federico")
                 .build();
+    }
+
+
+    public Connection<BankAccount> bankAccounts(int first, @Nullable String cursor) {
+
+        List<Edge<BankAccount>> edges = getBankAccounts(cursor)
+                .stream()
+                .limit(first)
+                .map(bankAccount -> {
+                    return new DefaultEdge<>(bankAccount, cursorUtil.createCursorWith(bankAccount.getId()));
+                })
+                .collect(Collectors.toList());
+
+        //in real implementation perform a query for hasPreviousPage (3) and hasNextPage (4)
+        var pageInfo = new DefaultPageInfo(cursorUtil.getFirstCursorFrom(edges),
+                cursorUtil.getLastCursorFrom(edges),
+                cursor != null, edges.size() >= first);
+
+        return new DefaultConnection<>(edges, pageInfo);
+    }
+
+
+    private List<BankAccount> getBankAccounts(String cursor) {
+        if(cursor == null) {
+            return bankAccountRepository.getBankAccounts();
+        }
+
+        return bankAccountRepository.getBankAccountsAfter(cursorUtil.decodeCursorWith(cursor));
     }
 
 }
